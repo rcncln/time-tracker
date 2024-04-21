d49c59eafd5dcbb42d82a7f0ae9b173afb959a34
rcncln <53015856+rcncln@users.noreply.github.com>
2024-04-21 13:59:10 +0200

    initial files

---
+++ app/signin/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Login } from '../../components/component/login-component'
+
+const Log = () => {
+  return (
+    <Login />
+  )
+}
+
+export default Log
---
+++ app/signup/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Signup } from '../../components/component/signup-component'
+
+export default function Page() {
+    return (
+        <Signup />
+    )
+}
