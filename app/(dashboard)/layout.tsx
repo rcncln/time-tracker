a3d5addf35eabd9216782aa48bd3bc6d95166d4d
rcncln <53015856+rcncln@users.noreply.github.com>
2024-04-28 12:31:49 +0200

    solved auth issue

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,19 @@
 import { Sidebar } from "@/components/component/sidebar";
+import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <Sidebar>{children}</Sidebar>;
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <Sidebar>{children}</Sidebar>
+      </SignedIn>
+    </header>
+  );
 }
